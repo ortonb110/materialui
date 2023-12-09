5643ed98c7c67cd05b0f0debac3edce585848738
Orton Bright <ortonb110@gmail.com>
2023-12-09 22:14:12 +0000

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Notes from './Pages/Notes'
+import Create from './Pages/Create'
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Notes/>}/>
+        <Route path='/create' element={<Create/>}/>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Pages/Notes.jsx
@@ -0,0 +1,9 @@
+
+
+const Notes = () => {
+  return (
+    <div>Notes</div>
+  )
+}
+
+export default Notes
