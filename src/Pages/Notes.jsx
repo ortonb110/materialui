80f6af07de66b5153c06fb5706692e6535f0cbba
Orton Bright <ortonb110@gmail.com>
2023-12-10 13:36:14 +0000

    Minor changes

--- src/Pages/Notes.jsx
+++ src/Pages/Notes.jsx
@@ -4,7 +4,7 @@ import { Typography } from "@mui/material"
 
 const Notes = () => {
   return (
-    <Typography variant="h1" component={'h2'}>
+    <Typography variant="h1" component={'h1'}>
       H1. Text
     </Typography>
   )
