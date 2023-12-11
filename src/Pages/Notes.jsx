657548568147dd2caf7cff8897455d5c4be19496
Orton Bright <ortonb110@gmail.com>
2023-12-11 20:27:32 +0000

    Minor changes

--- src/Pages/Notes.jsx
+++ src/Pages/Notes.jsx
@@ -1,13 +1,18 @@
-import { Typography } from "@mui/material"
-
-
+import { Typography } from "@mui/material";
 
 const Notes = () => {
   return (
-    <Typography variant="h1" component={'h1'}>
-      H1. Text
-    </Typography>
-  )
-}
+    <main>
+      <Typography variant="h1" component={"h1"}>
+        This is simple text from the Typography component from Material UI
+      </Typography>
+      <Typography variant="body" >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia reiciendis
+        rerum ex dolor, accusamus quibusdam laudantium unde inventore porro
+        doloribus, beatae eos! Maxime dolore, unde animi ut provident vitae et.
+      </Typography>
+    </main>
+  );
+};
 
-export default Notes
+export default Notes;
